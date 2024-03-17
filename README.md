9849cc4f6726557f9b3628865f993cc432d00fb5
Sebastian Pietras <me@spietras.dev>
2024-03-17 22:12:53 +0100

    Added initial project setup (#1)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# systems
+<h1 align="center">systems</h1>
+
+<div align="center">
+
 NixOS configs for our machines ❄️
+
+[![Lint](https://github.com/radio-aktywne/systems/actions/workflows/lint.yaml/badge.svg)](https://github.com/radio-aktywne/systems/actions/workflows/lint.yaml)
+[![Docs](https://github.com/radio-aktywne/systems/actions/workflows/docs.yaml/badge.svg)](https://github.com/radio-aktywne/systems/actions/workflows/docs.yaml)
+
+</div>
+
+---
+
+## 💡 About
+
+This repository contains [`NixOS`](https://nixos.org)
+configurations for all our machines.
+
+## 📄 Further Reading
+
+More in-depth documentation can be found
+[here](https://radio-aktywne.github.io/systems).
+
+## 💻 Development
+
+Read more about how to develop the project
+[here](https://github.com/radio-aktywne/systems/blob/main/CONTRIBUTING.md).
---
+++ flake.nix
@@ -0,0 +1,155 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = _inputs: let
+    # Add custom local inputs
+    inputs =
+      _inputs
+      // {
+        packages = import ./src/packages;
+        utils = import ./src/utils;
+      };
+  in
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      # Import local override if it exists
+      imports = [
+        (
+          if builtins.pathExists ./local.nix
+          then ./local.nix
+          else {}
+        )
+      ];
+
+      # System-specific configuration
+      flake = inputs.utils.mkHosts {
+        inherit inputs;
+        directory = "hosts";
+        hosts = [];
+      };
+
+      # Sensible defaults
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        node = pkgs.nodejs;
+        nil = pkgs.nil;
+        task = pkgs.go-task;
+        coreutils = pkgs.coreutils;
+        trunk = pkgs.trunk-io;
+        copier = pkgs.copier;
+        sops = pkgs.sops;
+      in {
+        # Override pkgs argument
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          config = {
+            # Allow packages with non-free licenses
+            allowUnfree = true;
+            # Allow packages with broken dependencies
+            allowBroken = true;
+            # Allow packages with unsupported system
+            allowUnsupportedSystem = true;
+          };
+        };
+
+        # Set which formatter should be used
+        formatter = pkgs.alejandra;
+
+        # Define multiple development shells for different purposes
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dev";
+
+            packages = [
+              node
+              nil
+              task
+              coreutils
+              trunk
+              copier
+              sops
+            ];
+
+            shellHook = ''
+              export TMPDIR=/tmp
+            '';
+          };
+
+          template = pkgs.mkShell {
+            name = "template";
+
+            packages = [
+              task
+              coreutils
+              copier
+            ];
+
+            shellHook = ''
+              export TMPDIR=/tmp
+            '';
+          };
+
+          lint = pkgs.mkShell {
+            name = "lint";
+
+            packages = [
+              node
+              task
+              coreutils
+              trunk
+            ];
+
+            shellHook = ''
+              export TMPDIR=/tmp
+            '';
+          };
+
+          docs = pkgs.mkShell {
+            name = "docs";
+
+            packages = [
+              node
+              task
+              coreutils
+            ];
+
+            shellHook = ''
+              export TMPDIR=/tmp
+            '';
+          };
+        };
+      };
+    };
+}
